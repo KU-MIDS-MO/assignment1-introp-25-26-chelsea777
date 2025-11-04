b730be5c52bbb69017d8f64c921fcc8e4297cc0a
Jiaqi Huang <sea.chelsea@hotmail.com>
2025-11-04 11:56:09 +0100

    Assignment is done

--- count_digits_greater_than.py
+++ count_digits_greater_than.py
@@ -1,4 +1,22 @@
 def count_digits_greater_than(n, t):
     ### Replace with your own code (begin) ###
-    pass
+    if type(n) != int or n<0:
+        return -1
+    if type(t) !=int or t<0 or t>9:
+        return -1
+    count = 0
+    
+    if n==0:
+        if 0 > t:
+            return 1
+        else:
+            return 0
+        
+    while n>0:
+        digit = n% 10
+        if digit >t:
+            count +=1
+        n= n // 10
+    return count
+        
     ### Replace with your own code (end)   ###
--- is_strictly_increasing_digits.py
+++ is_strictly_increasing_digits.py
@@ -1,4 +1,21 @@
 def is_strictly_increasing_digits(n):
     ### Replace with your own code (begin) ###
-    pass
+    if type(n)!=int or n <0:
+        return -1
+    if n < 10:
+        return True
+    
+    last = 10
+    while n > 0:
+        d= n%10
+        if d>=last:
+            return False
+        last = d
+        n //=10
+    return True
+    
+
+
+
+
     ### Replace with your own code (end)   ###
